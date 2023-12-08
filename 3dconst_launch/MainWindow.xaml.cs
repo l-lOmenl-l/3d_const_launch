7e4e4e82e8a264023a2bf2b627e8483518e5e0b6
Nikolai <pushkarev.n@e-1.ru>
2023-12-08 17:22:17 +0300

    first_commit

---
+++ 3dconst_launch/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3dconst_launch
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        static System.Windows.Controls.Label ui_TB_Message;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ui_TB_Message = TB_Message;
+            TB_Message.Content = "Start";
+            config.init();
+            
+        }
+
+        public static void const_actual()
+        {
+            ui_TB_Message.Content = "Actual";
+        }
+
+        public static void const_update()
+        {
+            ui_TB_Message.Content = "update";
+        }
+
+        public static void const_download()
+        {
+            ui_TB_Message.Content = "download";
+        }
+
+        static public void ChangeMessage(string msg)
+        {
+            ui_TB_Message.Content = msg;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton==MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ 3dconst_launch/config.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace _3dconst_launch
+{
+    internal class config
+    {
+        public class Conf
+        {
+            public string Local_version {  get; }
+            public bool Setup { get; }
+
+            public Conf(string local_version, bool setup)
+            {
+                Local_version = local_version;
+                Setup = setup;
+            }
+
+            public bool GetSetup()
+            {
+                return Setup;
+            }
+
+            public string GetLocalVersion()
+            {
+                return Local_version;
+            }
+
+        }
+
+        static public Conf conf = new Conf("", false);
+
+
+        static public string GetPathConfig()
+        {
+            return Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "/Launcher_E1/conf.json"; ;
+        }
+
+        static public void createConf()
+        {
+            conf = new Conf("none", false);
+            FileStream createStream = File.Create(GetPathConfig());
+            JsonSerializer.SerializeAsync(createStream, conf);
+            createStream.Dispose();
+        }
+
+        static public void readConf()
+        {
+            FileStream readStream = File.OpenRead(GetPathConfig());
+            conf = JsonSerializer.Deserialize<Conf>(readStream);
+        }
+
+       static public void init()
+        {
+            if (!File.Exists(GetPathConfig())){
+                createConf();
+            }
+
+            readConf();
+
+            if (conf.GetLocalVersion() == "none")
+            {
+                MainWindow.const_download();
+            } 
+
+
+
+        }
+
+
+
+    }
+}
